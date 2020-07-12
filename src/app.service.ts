790698df868f111530c59a5bed4466a3e9079565
GJCHOWDARY <chowdary716@gmail.com>
2020-07-12 20:54:30 +0530

    swagger api implemented

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hi.... <br/><b>API Reference Visit Swagger : http://localhost:3000/api/ </b>`
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,37 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as rateLimit from 'express-rate-limit';
 import * as compression from 'compression';
-import * as helmet from 'helmet'; 
+import * as helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('NestJS APP')
+    .setDescription(`Links API's`)
+    .setVersion('1.0')
+    .addTag('NestJS')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
-  const configService=app.get(ConfigService)
-  const port=configService.get('PORT');
+
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT');
+
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
+      max: 100, // To limit each IP to 100 requests per windowMs
     }),
   );
-   app.use(helmet());
+
+  app.use(helmet());
   //  app.enableCors();
-   app.use(compression());
-   await app.listen(port);
+  app.use(compression());
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
