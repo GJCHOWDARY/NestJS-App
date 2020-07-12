b80cd351b566f288e883a52586875a7e84536c2f
GJCHOWDARY <chowdary716@gmail.com>
2020-07-12 21:00:30 +0530

    swagger api implemented

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hi.... <br/><b>API Reference Visit Swagger : http://localhost:3000/api/ </b>`
+    return `<center>Hi.... <br/><br/><b>For API Reference (Swagger) : <a href="http://localhost:3000/api/#/">http://localhost:3000/api/ </a></b></center>`
   }
 }
