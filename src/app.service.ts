829659ff07f6a3085af7decdb5099e8d49aabf7d
GJCHOWDARY <chowdary716@gmail.com>
2020-07-12 21:48:35 +0530

    changes in main.ts and service files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService:ConfigService) {}
+ IP=this.configService.get('IP');
+ PORT=this.configService.get('PORT')
+
   getHello(): string {
-    return `<center>Hi.... <br/><br/><b>For API Reference (Swagger) : <a href="http://localhost:3000/api/#/">http://localhost:3000/api/ </a></b></center>`
+    return `<center>Hi.... <br/><br/><b>For API Reference (Swagger) : <a href="http://${this.IP}:${this.PORT}/api/#/">http://${this.IP}:${this.PORT}/api/ </a></b></center>`
   }
 }
